b4862b83782ee89a1062351e9f0eb870d02245f0
michel-heon <heon.michel@uqam.ca>
2023-01-17 14:28:14 +0000

    Ajout du script de liste des plateformes disponibles pour EBS

---
+++ script/ebs-list-platform-title.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+###################################################################
+# Script Name   :
+# Description   :
+# Args          : 
+# Author        : Michel Héon PhD
+# Institution   : Université du Québec à Montréal
+# Copyright     : Université du Québec à Montréal (c) 2023
+# Email         : heon.michel@uqam.ca
+###################################################################
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P)"
+
+aws elasticbeanstalk list-platform-versions --query 'PlatformSummaryList[].PlatformBranchName' 
+
